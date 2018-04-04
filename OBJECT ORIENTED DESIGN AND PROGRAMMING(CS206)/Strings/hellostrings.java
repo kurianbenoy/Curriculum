f5da4e7fdf1070911ea9d7692aed341b63e9837c
kurianbenoy <kurian.pro@gmail.com>
2018-04-04 10:44:05 -0500

    Add OODP folder

---
+++ OBJECT ORIENTED DESIGN AND PROGRAMMING(CS206)/Strings/hellostrings.java
@@ -0,0 +1,9 @@
+class hellostrings{
+	public static void main(String[] args) {	
+		String s = new String();
+		char c[] ={'J','a','v','a'};
+		String s1 = new String(c);
+		System.out.println(s);
+		System.out.println(c);
+		}
+} 
